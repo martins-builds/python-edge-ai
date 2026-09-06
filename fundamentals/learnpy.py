64da80e20c843cd8f73cbef9433a293fedb5dcc9
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-06 10:13:53 +0100

    Update learnpy.py

--- fundamentals/learnpy.py
+++ fundamentals/learnpy.py
@@ -1,7 +1,13 @@
 item = 'Banana'
 Item = 'Apple'
 Item_name = 'Orange'
+integer = 1234
+isHappy = False
+naughty_list = ['martins', 'oviawe', 'osayuki']
 
 print(item, Item)
 
 print('Hello ' + Item_name)
+print(integer)
+print(isHappy)
+print(naughty_list)
