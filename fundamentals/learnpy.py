5a8b2f418864d0820be81e2f86496dab7a9ebbca
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-06 10:39:43 +0100

    Update learnpy.py

--- fundamentals/learnpy.py
+++ fundamentals/learnpy.py
@@ -25,3 +25,11 @@ print(isHappy)
 print(naughty_list)
 
 say_hello('Martins')
+
+number = input('please provide a number >> ')
+
+try:
+    print(10 + int(number))
+
+except:
+    print('That is not a valid Number!')
