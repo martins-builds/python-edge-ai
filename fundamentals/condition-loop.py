4801545426e8369833d2fd7633b120915a123def
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-10 13:53:08 +0100

    Update condition-loop.py

--- fundamentals/condition-loop.py
+++ fundamentals/condition-loop.py
@@ -3,7 +3,7 @@ if n >= 100:
     print(bool(n))
 else:
     print(bool(n))
-    
+
 # Read two numbers
 number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
@@ -17,3 +17,10 @@ else:
 # Print the result
 print("The larger number is:", larger_number)
 
+x = [1,2,3,4,56,7,89,9887,4,34,543,435,434,3454,1000]
+largest = x[0]
+print(largest)
+for i in range (10):
+    if largest < x[i]:
+        largest = x[i]
+print(largest)
