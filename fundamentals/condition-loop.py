7da32d90bdf4b2965912368413d6a734c4a94cba
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-10 13:48:33 +0100

    Update condition-loop.py

--- fundamentals/condition-loop.py
+++ fundamentals/condition-loop.py
@@ -3,3 +3,17 @@ if n >= 100:
     print(bool(n))
 else:
     print(bool(n))
+    
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
