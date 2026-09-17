b2cbe28c36699ca9b5748e9c1cb36ac9b93ce468
MARTINS OVIAWE <martinsoviawe.dev@gmail.com>
2026-09-17 10:58:29 +0100

    Update datastructure.py

--- fundamentals/datastructure.py
+++ fundamentals/datastructure.py
@@ -36,15 +36,22 @@ print(m.keys())
 print(m.values())
 print(m.items())
 
-my_list = [8, 10, 6, 2, 4]  # list to sort
-swapped = True  # To enter the while loop.
+my_list = []
+swapped = True
+num = int(input("How many elements do you want to sort: "))
+
+for i in range(num):
+    val = float(input("Enter a list element: "))
+    my_list.append(val)
 
 while swapped:
-    swapped = False  # no swaps so far
+    swapped = False
     for i in range(len(my_list) - 1):
         if my_list[i] > my_list[i + 1]:
-            swapped = True  # a swap occurred!
+            swapped = True
             my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
 
+print("\nSorted:")
 print(my_list)
+
 
